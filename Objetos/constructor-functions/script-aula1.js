97a027606e2d535d495f0f66c2f18cd3d237ba98
M4Barboza <matheus.barboza0@gmail.com>
2021-12-15 13:51:52 -0300

    15/12/2021

--- Objetos/constructor-functions/script-aula1.js
+++ Objetos/constructor-functions/script-aula1.js
@@ -10,3 +10,13 @@ honda.preco = 4000;
 const fiat = new Carro();
 fiat.marca = "Fiat";
 fiat.preco = 3000;
+
+
+function Carro2(marca, precoInicial) {
+  const taxa = 1.2;
+  const precoFinal = precoInicial * this.taxa;
+  this.marca = marca;
+  this.preco = precoFinal;
+}
+
+const mazda = new Carro2('Mazda', 5000);
