c30ffdd0b46d7569130a8a6714df284ce5c716b9
M4Barboza <matheus.barboza0@gmail.com>
2021-12-16 09:38:35 -0300

    16/12/2021 - manhã

--- Objetos/constructor-functions/script.js
+++ Objetos/constructor-functions/script.js
@@ -1,23 +1,50 @@
-/* const Dom = {
-  seletor: 'li',
-  element() {
-    return document.querySelector(this.seletor);
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+  nome: "Nome pessoa",
+  idade: 0,
+  andar() {
+    console.log(this.nome + " andou");
   },
-  ativar() {
-    this.element().classList.add('ativar');
-  }
-} */
+};
+
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function () {
+    console.log(this.nome + " andou");
+  };
+}
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+
+const joão = new Pessoa("João", 20);
+const maria = new Pessoa("Maria", 25);
+const Bruna = new Pessoa("Bruna", 15);
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elemento
+
 function Dom(seletor) {
-  this.element = function () {
-    return document.querySelector(seletor);
-  }
-  this.ativar = function (classe) {
-    this.element().classList.add(classe);
-  }
+  //constructor fucntion com parametro do seletor que vamos pegar ao criar um novo objeto
+  const elementList = document.querySelectorAll(seletor); //armazenando o seletor na constante elementList
+  this.elements = elementList;
+  console.log(this.elements);
+  this.addClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    });
+  };
+  this.removeClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    });
+  };
 }
 
-const li = new Dom('li');
-const ul = new Dom('ul');
-
-const lastLi = new Dom('li:last-child');
-lastLi.ativar('ativar');
+const listaItens = new Dom("li");
+const ul = new Dom("ul");
