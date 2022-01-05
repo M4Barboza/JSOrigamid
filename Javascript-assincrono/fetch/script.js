96c44bbdb145c982fe5ad2fde9f2385bb74eaa79
M4Barboza <matheus.barboza0@gmail.com>
2022-01-04 21:43:46 -0300

    fetch 1

--- Javascript-assincrono/fetch/script.js
+++ Javascript-assincrono/fetch/script.js
@@ -1,7 +1,12 @@
-const doc = fetch('./doc.txt');
+const sobre = fetch('./sobre.html');
+const div = document.createElement('div');
 
-doc.then(resolucao => {
-    console.log(resolucao)
-})
 
-//minuto -12:36
+sobre.then(r => r.text())
+    .then((body) => {
+        div.innerHTML = body;
+        const titulo = div.querySelector('h1');
+        document.querySelector('h1').innerText = titulo.innerText;
+        console.log(titulo)
+    })
+
