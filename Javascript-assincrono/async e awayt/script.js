f5bb0f58ca8d0936cd7e40a95f851ec3b036f39c
M4Barboza <matheus.barboza0@gmail.com>
2022-01-06 13:09:06 -0300

    06/01/2022

--- Javascript-assincrono/async e awayt/script.js
+++ Javascript-assincrono/async e awayt/script.js
@@ -8,14 +8,17 @@
 puxarDados(); */
 
 async function puxarDados() {
-    try {
-        const responseDados = await fetch('./dados.json');
-        const jsonDados = await responseDados.json();
 
-        document.body.innerText = jsonDados.aula;
-    } catch (erro) {
-        console.log(erro);
-    }
+    const responseDados = fetch('./dados.json');
+    const responseClientes = fetch('./clients.json');
+
+
+    const jsonDados = await (await responseDados).json();
+    const jsonClientes = await (await responseClientes).json();
+
+    console.log(jsonDados)
+    console.log(jsonClientes)
+
 }
 
 puxarDados();
